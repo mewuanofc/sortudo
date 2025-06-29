78c33f23f8cf77241ba3934616a736796ad1b957
mewuanofc <mewuan@gmail.com>
2025-06-29 15:50:43 -0400

    Add files via upload

---
+++ sortudo.py
@@ -0,0 +1,7 @@
+import random
+
+nomes = ["Luan", "Lucas", "Maria", "Jordan", "Malu", "Pedro", "Julia", "Marcos", "Julio", "Mario", "Luigi", "Beatriz", "Carlos"]
+
+nome_sorteado = random.choice(nomes)
+
+print(f"O nome sorteado foi {nome_sorteado}")
